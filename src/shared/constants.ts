979191dd513a971fb9e50d6e803dcf84fe5dbffc
Sunggat Alimbetov <sunggat@greyball.com>
2025-05-17 21:41:54 +0500

    feat: guider component

---
+++ src/shared/constants.ts
@@ -0,0 +1,16 @@
+import type { SelectorItem } from './types';
+
+export const routemapSelectors: Array<SelectorItem> = [
+  {
+    text: 'В городе',
+    active: true,
+  },
+  {
+    text: 'За городом',
+    active: false,
+  },
+  {
+    text: 'Кухня',
+    active: false,
+  },
+];
---
+++ src/shared/types.ts
@@ -0,0 +1,1 @@
+export type SelectorItem = { text: string; active: boolean };
